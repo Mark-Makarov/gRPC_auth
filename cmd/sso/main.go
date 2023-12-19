27883ffe0911b5e9ed9e5d8b6b3281750c027b81
Mark-Makarov <mark.makararov@yandex.ru>
2023-12-20 02:50:27 +0300

    add config; gRPC, logs; linting lib

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,5 +1,40 @@
-package sso
+package main
+
+import (
+	"log/slog"
+	"os"
+	"sso/internal/app"
+	"sso/internal/config"
+)
+
+const (
+	envDev  = "dev"
+	envProd = "prod"
+)
 
 func main() {
+	cfg := config.MustLoad()
 
+	log := setupLogger(cfg.Env)
+	log.Info("app started on port", slog.Int("port", cfg.GRPC.Port))
+
+	application := app.New(log, cfg.GRPC.Port, cfg.StoragePath, cfg.TokenTTL)
+	application.GRPCSrv.MustRun()
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envDev:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
---
+++ internal/config/config.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"flag"
+	"github.com/ilyakaznacheev/cleanenv"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env         string        `yaml:"env" env-default:"dev"`
+	StoragePath string        `yaml:"storage_path" env-required:"./data"`
+	TokenTTL    time.Duration `yaml:"token_ttl" env-default:"1h"`
+	GRPC        GRPCConfig    `yaml:"grpc"`
+}
+
+type GRPCConfig struct {
+	Port    int           `yaml:"port"`
+	Timeout time.Duration `yaml:"timeout"`
+}
+
+func MustLoad() *Config {
+	path := fetchConfigPath()
+	if path == "" {
+		panic("config path is empty")
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("config file does not found, path to config: " + path)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+		panic("error read config: " + err.Error())
+	}
+
+	return &cfg
+}
+
+func fetchConfigPath() string {
+	var res string
+
+	// --config="path/to/config.yaml"
+	flag.StringVar(&res, "config", "", "path to config")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
+}
